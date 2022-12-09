0a0b02e2a23a774eb7bd93e1d2d21aacff9b967c
ChoNatalya <114421286+ChoNatalya@users.noreply.github.com>
2022-12-10 00:29:24 +0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,12 +1,12 @@
 <?php
-if (isset($_GET['id'])){ //($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $xml = simplexml_load_file("data.xml");// or die("Error: Cannot create object");
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $xml = simplexml_load_file("data.xml");
     $id = $_GET['id'];
     $i = 0;
     foreach ($xml->plant as $plant) {
         if ($plant['id'] == $id) {
             unset($xml->plant[$i]);
-            // обновляет теги элементов
+            
             $i = 1;
             foreach ($xml->plant as $plant) {
                 $plant['id'] = $i++;
